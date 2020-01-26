0939d7ec44af32c05405412f6afe609ea468034b
Da Chen <da6@ksu.edu>
2020-01-25 21:03:09 -0600

    click

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -37,11 +37,11 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(251, 116);
+            this.textBox1.Location = new System.Drawing.Point(181, 116);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(293, 235);
+            this.textBox1.Size = new System.Drawing.Size(415, 251);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
